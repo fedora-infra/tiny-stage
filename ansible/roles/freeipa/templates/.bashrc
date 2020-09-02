2b78bfcc8a4e030220bbadf418c625386009c860
Ryan Lerch <rlerch@redhat.com>
2020-09-02 21:42:08 +1000

    fix dummy data script, and run on provisioning
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- ansible/roles/freeipa/templates/.bashrc
+++ ansible/roles/freeipa/templates/.bashrc
@@ -1,3 +1,3 @@
 # .bashrc
 alias tinystage-ipa-resetdb="sudo ipa-restore /var/lib/ipa/backup/backup-clean -p {{ ipa_admin_password }}"
-alias tinystage-ipa-populatedb="poetry run python /vagrant/devel/create-test-data.py"
+alias tinystage-ipa-populatedb="python /home/vagrant/create_dummy_data.py"
--- ansible/roles/freeipa/templates/create_dummy_data.py
+++ ansible/roles/freeipa/templates/create_dummy_data.py
@@ -3,16 +3,19 @@ from faker import Faker
 
 import python_freeipa
 
-USER_PASSWORD = "testuserpw"
+USER_PASSWORD = "password"
 
 fake = Faker()
 fake.seed_instance(0)
 
 ipa = python_freeipa.ClientLegacy(
-    host="ipa.example.test", verify_ssl="/etc/ipa/ca.crt"
+    host="ipa.tinystage.test", verify_ssl="/etc/ipa/ca.crt"
 )
 ipa.login("{{ ipa_admin_user }}", "{{ ipa_admin_password }}")
 
+untouched_ipa = python_freeipa.ClientLegacy(
+    host="ipa.tinystage.test", verify_ssl="/etc/ipa/ca.crt"
+)
 
 # create a developers fasgroup
 try:
@@ -31,19 +34,24 @@ for x in range(50):
     lastName = fake.last_name()
     username = firstName + lastName
     fullname = firstName + " " + lastName
-    print(f"adding user {fullname}")
+    print(f"adding user {username} - {fullname}")
     try:
         ipa.user_add(
             username,
             firstName,
             lastName,
             fullname,
+            disabled=False,
+            user_password=USER_PASSWORD,
             fasircnick=[username, username + "_"],
             faslocale="en-US",
             fastimezone="Australia/Brisbane",
             fasstatusnote="active",
             fasgpgkeyid=[],
         )
+
+        untouched_ipa.change_password(username, new_password=USER_PASSWORD, old_password=USER_PASSWORD)
+
         if x % 3 == 0:
             ipa.group_add_member("developers", username)
             if x < 10:
