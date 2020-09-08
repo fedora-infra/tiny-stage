1d10bab070761073b8ad1c22ce6de20afba4154a
Ryan Lerch <rlerch@redhat.com>
2020-09-08 19:42:49 +1000

    make the attributes all map properly

--- ansible/roles/ipsilon/files/account-scopes.py
+++ ansible/roles/ipsilon/files/account-scopes.py
@@ -7,16 +7,12 @@ class OpenidCExtension(OpenidCExtensionBase):
     name = 'fedora-account'
     display_name = 'Fedora Account Information'
     scopes = {
-        'fedora': {  # NOTE: This is temporary! DO NOT USE IN NEW PROJECTS!
-            'display_name': 'Fedora',
-            'claims': ['cla', 'zoneinfo', 'groups']
-        },
         'https://id.fedoraproject.org/scope/groups': {
-            'display_name': 'Fedora Account Groups list',
+            'display_name': 'Fedora Account Group Memberships',
             'claims': ['groups']
         },
-        'https://id.fedoraproject.org/scope/cla': {
-            'display_name': 'Fedora Account CLA status',
-            'claims': ['cla']
+        'https://id.fedoraproject.org/scope/agreements': {
+            'display_name': 'Fedora Account Signed Agreements',
+            'claims': ['agreements']
         },
     }
