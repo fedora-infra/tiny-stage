93a2030d5be6ad25d2166aac2f7d501057ec0dee
Ryan Lerch <rlerch@redhat.com>
2020-09-16 15:27:32 +1000

    Extend the userdata script
    Adds agreements, and more users and groups.
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- ansible/roles/freeipa/templates/create_dummy_data.py
+++ ansible/roles/freeipa/templates/create_dummy_data.py
@@ -8,6 +8,24 @@ USER_PASSWORD = "password"
 fake = Faker()
 fake.seed_instance(0)
 
+
+def rando(percentage):
+    if fake.random_int(min=0, max=100, step=1) < percentage:
+        return True
+    else:
+        return False
+
+
+groups = {
+    "developers": 80,
+    "designers": 30,
+    "elections": 10,
+    "infra": 20,
+    "QA": 20,
+    "translators": 10,
+    "ambassadors": 30,
+}
+
 ipa = python_freeipa.ClientLegacy(
     host="ipa.tinystage.test", verify_ssl="/etc/ipa/ca.crt"
 )
@@ -17,19 +35,17 @@ untouched_ipa = python_freeipa.ClientLegacy(
     host="ipa.tinystage.test", verify_ssl="/etc/ipa/ca.crt"
 )
 
-# create a developers fasgroup
-try:
-    ipa.group_add("developers", "A group for developers", fasgroup=True)
-except python_freeipa.exceptions.FreeIPAError as e:
-    print(e)
+ipa._request("fasagreement_add", "FPCA", {"description": "This ia the FPCA agreement"})
 
-# create a designers fasgroup
-try:
-    ipa.group_add("designers", "A group for designers", fasgroup=True)
-except python_freeipa.exceptions.FreeIPAError as e:
-    print(e)
 
-for x in range(50):
+for group in groups.keys():
+    ipa.group_add(group, f"A group for {group}", fasgroup=True)
+    ipa._request("fasagreement_add_group", "FPCA", {"group": group})
+
+ipa.group_add("general", f"A group for general stuff", fasgroup=True)
+
+
+for x in range(100):
     firstName = fake.first_name()
     lastName = fake.last_name()
     username = firstName + lastName
@@ -50,24 +66,26 @@ for x in range(50):
             fasgpgkeyid=[],
         )
 
-        untouched_ipa.change_password(username, new_password=USER_PASSWORD, old_password=USER_PASSWORD)
+        untouched_ipa.change_password(
+            username, new_password=USER_PASSWORD, old_password=USER_PASSWORD
+        )
 
-        if x % 3 == 0:
-            ipa.group_add_member("developers", username)
-            if x < 10:
-                ipa._request(
-                    "group_add_member_manager",
-                    "developers",
-                    {"user": username},
-                )
-        if x % 5 == 0:
-            ipa.group_add_member("designers", username)
-            if x <= 15:
-                ipa._request(
-                    "group_add_member_manager",
-                    "designers",
-                    {"user": username},
-                )
+        has_signed_fpca = False
+        if rando(90):
+            ipa._request("fasagreement_add_user", "FPCA", {"user": username})
+            has_signed_fpca = True
+        else:
+            ipa.group_add_member("general", username)
+
+        # add to groups
+        for groupname, chance in groups.items():
+            if rando(chance) and has_signed_fpca:
+                ipa.group_add_member(groupname, username)
+                # add member manager (sponsor)
+                if rando(30):
+                    ipa._request(
+                        "group_add_member_manager", groupname, {"user": username}
+                    )
 
     except python_freeipa.exceptions.FreeIPAError as e:
         print(e)
