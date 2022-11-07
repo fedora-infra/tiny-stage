04d2b6d0286c63383ca2e34b1a4b9f6f44282176
Ryan Lerch <rlerch@redhat.com>
2022-11-07 13:55:43 +1000

    update pagure dev-data script to generate some projects
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- ansible/roles/pagure/files/dev-data.py
+++ ansible/roles/pagure/files/dev-data.py
@@ -14,12 +14,7 @@ from sqlalchemy.exc import IntegrityError
 
 _config = pagure.config.reload_config()
 
-
-# userdata = [
-#     { "username":"aaronhale", "fullname": "Aaron Hale", "email": "aaronhale@tinystage.test", "groups":["designers", "developers"]},
-#     { "username":"alexandraclark", "fullname": "Alexandra Clark", "email": "alexandraclark@tinystage.test", "groups":["developers"]},
-
-# ]
+userslist = []
 
 from fasjson_client import Client
 c = Client('http://fasjson.tinystage.test/fasjson', principal='admin@TINYSTAGE.TEST')
@@ -30,6 +25,7 @@ def insert_data(session):
 
     userdata = c.list_users().result
     for user in userdata:
+        userslist.append(user['username'])
         u = pagure.lib.model.User(
             user=user['username'],
             fullname=user['human_name'],
@@ -78,7 +74,43 @@ def insert_data(session):
                 session.commit()
             except IntegrityError:
                 session.rollback()
-            
+
+    projects = ["0ad-data", "0install", "0xFFFF", "2048-cli", "2ping", "389-admin", "389-admin-console", "389-adminutil", "389-console", "389-directory-server", "389-ds", "389-ds-base", "389-ds-console", "389-dsgw", "3Depict", "3dprinter-udev-rules", "3mux", "3proxy", "4diac-forte", "4Pane", "4th", "4ti2", "5minute", "64tass", "6tunnel", "7kaa", "8Kingdoms", "8sync", "90-Second-Portraits", "915resolution", "99soft-oss-parent", "9wm", "a2jmidid", "a2ps", "a52dec", "aajohan-comfortaa-fonts", "aalib", "aalto-xml", "aardvark-dns", "aasaver", "abakus", "abattis-cantarell-fonts", "abbayedesmorts-gpl", "abby", "abc", "abcde", "abcm2ps", "abcMIDI", "abduco", "abe", "abgraph", "abicheck", "abi-compliance-checker", "abi-dumper", "abi-tracker", "abiword", "abook", "aboot", "abootimg", "abrt", "abrt-addon-python3", "abrt-java-connector", "abrt-server-info-page", "abseil-cpp", "abuse", "abyssinica-fonts", "academic-admin", "accel-config", "accerciser", "access-modifier-annotation", "accountsdialog", "accounts-qml-module", "accountsservice", "accrete", "accumulo", "acd_cli", "ace", "acegisecurity", "aces_container", "AcetoneISO", "AcetoneISO2", "acheck", "acheck-rules", "ack", "acl", "acme-tiny", "acpi", "acpica-tools", "acpid", "acpitool", "act", "activeio", "activemq", "activemq-cpp", "activemq-protobuf", "adanaxisgpl", "adapt"]
+    namespaces = ['rpms', 'containers', 'modules', 'flatpaks']
+    for project in projects:
+        thisusername = userslist[int.from_bytes(bytes(project,'utf-8'), byteorder='big') % 10 ]
+        namespace = namespaces[int.from_bytes(bytes(project,'utf-8'), byteorder='big') % len(namespaces) ]
+        u = pagure.lib.query.get_user(
+            session, key=thisusername
+        )
+
+        p = pagure.lib.model.Project(
+            user_id=u.id,
+            name=project,
+            is_fork=False,
+            parent_id=None,
+            description=f"{project} {namespace}",
+            namespace=namespace,
+            hook_token=project
+        )
+        p.close_status = ["Invalid", "Insufficient data", "Fixed", "Duplicate"]
+        try:
+            print(f"adding {project} for user {thisusername} in namespace {namespace}")
+            session.add(p)
+            session.commit()
+        except IntegrityError as e:
+            print(e)
+            print("error")
+            session.rollback()
+
+
+        # group = pagure.lib.query.search_groups(
+        #     session, pattern=None, group_name="rel-eng", group_type=None
+        # )
+        # repo = pagure.lib.query.get_authorized_project(session, "test")
+        # item = pagure.lib.model.ProjectGroup(
+        #     project_id=repo.id, group_id=group.id, access="commit"
+        # )
 
 
 if __name__ == "__main__":
